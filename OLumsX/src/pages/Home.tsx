34ef46900e02f1068e5181ce6a97a7ae3e04d9de
Muhammad Shayan <129618428+m-shayan73@users.noreply.github.com>
2024-03-04 00:07:03 +0500

    Update and rename home.tsx to Home.tsx

---
+++ OLumsX/src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import "../styles/home.scss";
+import sse_main from "../assets/images/sse_main.png";
+import Header from "../components/HeaderMain";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-page">
+        <Header />
+
+        <img src={sse_main} alt="LUMS Market" id="sse-img" />
+
+        <div className="category-bar">
+          <div className="category-box">Grocery</div>
+
+          <div className="category-box">Tuition</div>
+
+          <div className="category-box">Gadgets</div>
+
+          <div className="category-box">Food</div>
+
+          <div className="category-box">Gifts</div>
+
+          <div className="category-box">Art</div>
+
+          <div className="category-box">Clothing</div>
+
+          <div className="category-box">Services</div>
+
+          <div className="category-box">Misc</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
