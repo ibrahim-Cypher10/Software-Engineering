28b45626b424e333e70e311a6c6c8be2ee001062
Muhammad Shayan <shayanhanif73@gmail.com>
2024-03-03 14:40:23 +0500

    Update App.tsx

--- OLumsX/src/App.tsx
+++ OLumsX/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
-
 const App = () => {
-  return 
-  <>
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
