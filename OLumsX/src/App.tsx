05450bbbe42f8cdd9cbcde4ccd7cb4ee014e18fd
Muhammad Shayan <shayanhanif73@gmail.com>
2024-03-03 20:46:29 +0500

    Home Page

--- OLumsX/src/App.tsx
+++ OLumsX/src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCartShopping, faGear, faHeart, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons'
+import Home from './pages/Home';
+
+library.add(fab, faHeart, faUser, faCartShopping, faGear, faMagnifyingGlass)
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- OLumsX/src/pages/home.tsx
+++ OLumsX/src/pages/home.tsx
@@ -1,7 +1,37 @@
+import "../styles/home.scss";
+import sse_main from "../assets/images/sse_main.png";
+import Header from "../components/Header";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="home-page">
+        <Header />
 
-export default Home
+        <img src={sse_main} alt="LUMS Market" id="sse-img" />
+
+        <div className="category-bar">
+          <div className="category-box">Grocery</div>
+
+          <div className="category-box">Tuition</div>
+
+          <div className="category-box">Gadgets</div>
+
+          <div className="category-box">Food</div>
+
+          <div className="category-box">Gifts</div>
+
+          <div className="category-box">Art</div>
+
+          <div className="category-box">Clothing</div>
+
+          <div className="category-box">Services</div>
+
+          <div className="category-box">Misc</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
