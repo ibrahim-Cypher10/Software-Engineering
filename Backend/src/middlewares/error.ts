a33b9489adc2b2cf508b90151b71cec3d475936c
Ibrahim Ahmed Khan <155223063+ibrahim-Cypher10@users.noreply.github.com>
2024-03-03 23:13:48 +0500

    Update error.ts
    added comments - Ibrahim

--- Backend/src/middlewares/error.ts
+++ Backend/src/middlewares/error.ts
@@ -2,14 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/utility-class.js";
 import { ControllerType } from "../types/types.js";
 
-export const errorMiddleware = (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
-    
+// Defines middleware for handling errors in Express by setting default values for error messages and status codes, then responding with a JSON object containing these details.
+export const errorMiddleware = (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {    
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
 
     return res.status(err.statusCode).json({ success: false, message: err.message });
 }
 
+// The try catch block was being used repetitively, so i made this trycatch wrapper function that replaces the try catch block.
 export const TryCatch = (func: ControllerType) => {
     return (req: Request, res: Response, next: NextFunction) => {
         return Promise.resolve(func(req, res, next)).catch(next);
