ba0a579c08fed1e5a78528081b8fc0375ecfab3e
ibrahim-Cypher10 <25100112@lums.edu.pk>
2024-04-09 20:43:29 +0500

    Admin (Frontend)
    Admin Dashboard Frontend Completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') 
-);
-root.render(
-  <React.StrictMode>
+import App from "./App";
+
+import "./index.css";
+
+// Redux Store
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+// Rendering App
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
