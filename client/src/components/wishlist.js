bdd19ef3322332ee9fee951ff21c43ac616c370b
Muhammad Shayan <129618428+m-shayan73@users.noreply.github.com>
2024-04-12 17:08:15 +0500

    Delete client directory

--- client/src/components/wishlist.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-const Wishlist = () => {
-  return <>Wishlist</>;
-};
-
-export default Wishlist;
